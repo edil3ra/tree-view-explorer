61136b3c097d8c7838e9915b620be4890a56faa5
edil3ra <edil3ra@gmail.com>
2021-02-09 13:21:53 +0100

    Add models

--- collibra/models.py
+++ collibra/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+
+class NodeTypes(models.Model):
+    node_type_name = models.CharField(max_length=100)
+
+
+class Nodes(models.Model):
+    node_type = models.ForeignKey(NodeTypes, on_delete=models.CASCADE, related_name='nodes')
+    parent = models.ForeignKey('self', on_delete=models.CASCADE, related_name='children', null=True)
+    node_name = models.CharField(max_length=100)
+
+
+class Tables(models.Model):
+    node = models.ForeignKey(Nodes, on_delete=models.CASCADE, related_name='tables')
+    table_name = models.CharField(max_length=100)
+    description = models.TextField()
+
+
+class Columns(models.Model):
+    node = models.ForeignKey(Tables, on_delete=models.CASCADE, related_name='columns')
+    column_name = models.CharField(max_length=100)
+    description = models.TextField()
