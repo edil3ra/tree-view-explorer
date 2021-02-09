8f98e0df525e875f22feb7bf3220646dfe9532d0
edil3ra <edil3ra@gmail.com>
2021-02-09 14:56:09 +0100

    Add sqlite

--- collibra/admin.py
+++ collibra/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from .models import NodeTypes, Nodes, Tables, Columns
 
+
+
 admin.site.register(NodeTypes)
 admin.site.register(Nodes)
 admin.site.register(Tables)
--- collibra/models.py
+++ collibra/models.py
@@ -3,32 +3,60 @@ from django.db import models
 
 class NodeTypes(models.Model):
     node_type_name = models.CharField(max_length=100)
-    
+
+    def __str__(self):
+        return f'{self.node_type_name}'
+
     class Meta:
+        verbose_name = 'NodeType'
         verbose_name_plural = "NodeTypes"
 
+
 class Nodes(models.Model):
-    node_type = models.ForeignKey(NodeTypes, on_delete=models.CASCADE, related_name='nodes')
-    parent = models.ForeignKey('self', on_delete=models.CASCADE, related_name='children', null=True)
+    node_type = models.ForeignKey(NodeTypes,
+                                  on_delete=models.CASCADE,
+                                  related_name='nodes')
+    parent = models.ForeignKey('self',
+                               on_delete=models.CASCADE,
+                               related_name='children',
+                               blank=True,
+                               null=True)
     node_name = models.CharField(max_length=100)
 
+    # find a way to reference parent to improve display name
+    def __str__(self):
+        return f'{self.node_name}'
+
     class Meta:
+        verbose_name = 'Node'
         verbose_name_plural = "Nodes"
 
 
 class Tables(models.Model):
-    node = models.ForeignKey(Nodes, on_delete=models.CASCADE, related_name='tables')
+    node = models.ForeignKey(Nodes,
+                             on_delete=models.CASCADE,
+                             related_name='tables')
     table_name = models.CharField(max_length=100)
     description = models.TextField()
 
+    def __str__(self):
+        return f'{self.node} - {self.table_name}'
+
     class Meta:
+        verbose_name = 'Table'
         verbose_name_plural = "Tables"
 
 
 class Columns(models.Model):
-    node = models.ForeignKey(Tables, on_delete=models.CASCADE, related_name='columns')
+    node = models.ForeignKey(Tables,
+                             on_delete=models.CASCADE,
+                             related_name='columns')
     column_name = models.CharField(max_length=100)
     description = models.TextField()
 
+    def __str__(self):
+        return f'{self.node} - {self.column_name}'
+
     class Meta:
+        verbose_name = 'Column'
         verbose_name_plural = "Columns"
